ca271cc78a968c86d7f1f1aac6f0621d0b100e7d
Richard Pennington <rich@ellcc.org>
2014-05-24 13:23:37 +0000

    i386 simple colled I/O.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@4059 82857169-6e38-49d7-aaa8-07fb558f12fc

--- baremetal/i386/main.c
+++ baremetal/i386/main.c
@@ -4,13 +4,8 @@
 #include <stdio.h>
 #include <command.h>
 
-#define SIMPLE_CONSOLE
-#include "console.h"
 int main(int argc, char **argv)
 {
-    extern void apic_init(void);
-    apic_init();
-    init();
     puts("type control-A x to get out of QEMU");
     printf("%s started. Type \"help\" for a list of commands.\n", argv[0]);
     // Enter the kernel command processor.
