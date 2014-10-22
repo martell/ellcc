d170f9f4aea5015b2a69a078840021c3c2ac7d15
Richard Pennington <rich@ellcc.org>
2014-10-22 03:00:51 +0000

    Build compiler-rt with soft float.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@4316 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/mkscripts/targets/mips/setup.mk
+++ libecc/mkscripts/targets/mips/setup.mk
@@ -2,7 +2,7 @@ TARGET := mips
 Arch.$(TARGET) := mips
 Triple.$(TARGET) := $(TARGET)-ellcc-$(OS)
 TARGET.$(TARGET) := -target $(Triple.$(TARGET))
-ASFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 $(ASFLAGS)
-CFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 $(CFLAGS)
-CXXFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 $(CXXFLAGS)
+ASFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 -msoft-float $(ASFLAGS)
+CFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 -msoft-float $(CFLAGS)
+CXXFLAGS.$(TARGET) := $(TARGET.$(TARGET)) -mcpu=mips32r2 -msoft-float $(CXXFLAGS)
 LDFLAGS := $(TARGET.$(TARGET))
