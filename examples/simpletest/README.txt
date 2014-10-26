0d1c03f56582a065657709f8cf8eef19d635e774
Richard Pennington <rich@ellcc.org>
2014-10-26 20:48:05 +0000

    More examples.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@4333 82857169-6e38-49d7-aaa8-07fb558f12fc

---
+++ examples/simpletest/README.txt
@@ -0,0 +1,1 @@
+This is a simple thread and command test program.
---
+++ examples/simpletest/main.c
@@ -0,0 +1,13 @@
+/* A simple bare metal main.
+ */
+
+#include <stdio.h>
+#include "command.h"
+
+int main(int argc, char **argv)
+{
+    printf("%s started. Type \"help\" for a list of commands.\n", argv[0]);
+    // Enter the kernel command processor.
+    do_commands(argv[0]);
+}
+
