f8fa4fe1082e0685ed5c1a2e6238a1b0ffdf902a
Richard Pennington <rich@ellcc.org>
2014-11-09 15:41:57 +0000

    Don't export ELK internal include files.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@4394 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/elk/sources.mk
+++ libecc/src/elk/sources.mk
@@ -1,5 +1,6 @@
 CFLAGS += -g -Werror -Wall -std=c99 -D_XOPEN_SOURCE=700 -D_BSD_SOURCE
-CFLAGS += -I$(SRCPATH)/$(LIB)/include/$(ARCH) \
+CFLAGS += -I$(SRCPATH)/$(LIB)/sys/$(ARCH) \
+          -I$(SRCPATH)/$(LIB)/sys \
           -I$(SRCPATH)/$(LIB)/include
 
 VPATH := $(VPATH):$(SRCPATH)/$(LIB)/$(ARCH)
