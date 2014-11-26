7a7079ed6ad737b9264060cbb1dec88c6f93396c
Richard Pennington <rich@ellcc.org>
2014-11-26 15:26:59 +0000

    More MMU Handling.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@4513 82857169-6e38-49d7-aaa8-07fb558f12fc

---
+++ libecc/src/elk/sys/i386/cpu.h
@@ -0,0 +1,193 @@
+/*-
+ * Copyright (c) 2005, Kohsuke Ohtani
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _cpu_h_
+#define _cpu_h_
+
+// GDTs
+#define KERNEL_CS       0x10
+#define KERNEL_DS       0x18
+#define USER_CS         0x20
+#define USER_DS         0x28
+#define KERNEL_TSS      0x38
+
+#define NGDTS           8
+
+// IDTs
+#define NIDTS           0x41
+#define SYSCALL_INT     0x40
+#define INVALID_INT     0xFF
+
+// x86 flags register
+#define EFL_CF          0x00000001      // Carry.
+#define EFL_PF          0x00000004      // Parity.
+#define EFL_AF          0x00000010      // Carry.
+#define EFL_ZF          0x00000040      // Zero.
+#define EFL_SF          0x00000080      // Sign.
+#define EFL_TF          0x00000100      // Trap.
+#define EFL_IF          0x00000200      // Interrupt enable.
+#define EFL_DF          0x00000400      // Direction.
+#define EFL_OF          0x00000800      // Overflow.
+#define EFL_IOPL        0x00003000      // IO privilege level:
+#define EFL_IOPL_KERN   0x00000000      // Kernel.
+#define EFL_IOPL_USER   0x00003000      // User.
+#define EFL_NT          0x00004000      // Nested task.
+#define EFL_RF          0x00010000      // Resume without tracing.
+#define EFL_VM          0x00020000      // Virtual 8086 mode.
+#define EFL_AC          0x00040000      // Alignment Check.
+
+/*
+ * CR0 register
+ */
+#define CR0_PG          0x80000000      // Enable paging.
+#define CR0_CD          0x40000000      // Cache disable.
+#define CR0_NW          0x20000000      // No write-through.
+#define CR0_AM          0x00040000      // Alignment check mask.
+#define CR0_WP          0x00010000      // Write-protect kernel access.
+#define CR0_NE          0x00000020      // Handle numeric exceptions.
+#define CR0_ET          0x00000010      // Extension type is 80387 coprocessor.
+#define CR0_TS          0x00000008      // Task switch.
+#define CR0_EM          0x00000004      // Emulate coprocessor.
+#define CR0_MP          0x00000002      // Monitor coprocessor.
+#define CR0_PE          0x00000001      // Enable protected mode.
+
+#ifndef __ASSEMBLER__
+
+#include <sys/types.h>
+#include <context.h>
+
+#if defined(__SUNPRO_C)
+#pragma pack(1)
+#endif
+
+/** Segment Descriptor
+ */
+struct seg_desc
+{
+  u_int limit_lo:16;                    // Segment limit (lsb).
+  u_int base_lo:16;                     // Segment base address (lsb).
+  u_int base_mid:8;                     // Segment base address (middle).
+  u_int type:8;                         // Type.
+  u_int limit_hi:4;                     // Segment limit (msb).
+  u_int size:4;                         // Size.
+  u_int base_hi:8;                      // Segment base address (msb).
+} __packed;
+
+/** Gate Descriptor
+ */
+struct gate_desc
+{
+  u_int offset_lo:16;                   // Gate offset (lsb).
+  u_int selector:16;                    // Gate segment selector.
+  u_int nr_copy:8;                      // Stack copy count.
+  u_int type:8;                         // Type.
+  u_int offset_hi:16;                   // Gate offset (msb).
+} __packed;
+
+/** Linear memory description for lgdt and lidt instructions.
+ */
+struct desc_p
+{
+  uint16_t limit;
+  uint32_t base;
+} __packed;
+
+/** Segment size
+ */
+#define SIZE_32         0x4             // 32-bit segment.
+#define SIZE_16         0x0             // 16-bit segment.
+#define SIZE_4K         0x8             // 4K limit field.
+
+/** Segment type
+ */
+#define ST_ACC          0x01            // Accessed.
+#define ST_LDT          0x02            // LDT.
+#define ST_CALL_GATE_16 0x04            // 16-bit call gate.
+#define ST_TASK_GATE    0x05            // Task gate.
+#define ST_TSS          0x09            // Task segment.
+#define ST_CALL_GATE    0x0c            // Call gate.
+#define ST_INTR_GATE    0x0e            // Interrupt gate.
+#define ST_TRAP_GATE    0x0f            // Trap gate.
+
+#define ST_TSS_BUSY     0x02            // Task busy.
+
+#define ST_DATA         0x10            // Data.
+#define ST_DATA_W       0x12            // Data, writable.
+#define ST_DATA_E       0x14            // Data, expand-down.
+#define ST_DATA_EW      0x16            // Data, expand-down, writable.
+
+#define ST_CODE         0x18            // Code.
+#define ST_CODE_R       0x1a            // Code, readable.
+#define ST_CODE_C       0x1c            // Code, conforming.
+#define ST_CODE_CR      0x1e            // Code, conforming, readable.
+
+#define ST_KERN         0x00            // Kernel access only.
+#define ST_USER         0x60            // User access.
+
+#define ST_PRESENT      0x80            // Segment present.
+
+/** Task State Segment (TSS)
+ */
+
+#define IO_BITMAP_SIZE    (65536/8 + 1)
+#define INVALID_IO_BITMAP  0x8000
+
+struct tss
+{
+  uint32_t back_link;
+  uint32_t esp0, ss0;
+  uint32_t esp1, ss1;
+  uint32_t esp2, ss2;
+  uint32_t cr3;
+  uint32_t eip;
+  uint32_t eflags;
+  uint32_t eax, ecx, edx, ebx;
+  uint32_t esp, ebp, esi, edi;
+  uint32_t es, cs, ss, ds, fs, gs;
+  uint32_t ldt;
+  uint16_t dbg_trace;
+  uint16_t io_bitmap_offset;
+#if 0
+  uint32_t io_bitmap[IO_BITMAP_SIZE/4+1];
+  uint32_t pad[5];
+#endif
+} __packed;
+
+
+#if defined(__SUNPRO_C)
+#pragma pack()
+#endif
+
+void tss_set(uint32_t);
+uint32_t tss_get(void);
+void  cpu_init(void);
+
+#endif // !__ASSEMBLER__
+
+#endif // !_cpu_h_
---
+++ libecc/src/elk/sys/ppc/cpu.h
@@ -0,0 +1,92 @@
+/*-
+ * Copyright (c) 2009, Kohsuke Ohtani
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _cpu_h_
+#define _cpu_h_
+
+#define STKFRAME_LEN    8               // Length for stack frame.
+
+/** Flags in MSR
+ */
+#define MSR_LE          0x00000001      // Little-endian mode enable.
+#define MSR_RI          0x00000002      // Recoverable exception.
+#define MSR_DR          0x00000010      // Data address translation.
+#define MSR_IR          0x00000020      // Instruction address translation.
+#define MSR_IP          0x00000040      // Exception prefix.
+#define MSR_FE1         0x00000100      // Floating-point exception mode 1.
+#define MSR_BE          0x00000200      // Branch trace enable.
+#define MSR_SE          0x00000400      // Single-step trace enable.
+#define MSR_FE0         0x00000800      // Floating-point exception mode 0.
+#define MSR_ME          0x00001000      // Machine check enable.
+#define MSR_FP          0x00002000      // Floating-point available.
+#define MSR_PR          0x00004000      // Privilege level (1:USR).
+#define MSR_EE          0x00008000      // External interrupt enable.
+#define MSR_ILE         0x00010000      // Exception little-endian mode (1:LE).
+#define MSR_POW         0x00040000      // Power management enable.
+
+// Default msr for starting a user program.
+#if 0
+#ifdef CONFIG_MMU
+  #define MSR_DFLT  (uint32_t)(MSR_EE|MSR_PR|MSR_ME|MSR_IR|MSR_DR)
+#else
+  #define MSR_DFLT  (uint32_t)(MSR_EE|MSR_PR|MSR_ME)
+#endif
+#endif
+
+#define MSR_DFLT  (uint32_t)(MSR_EE|MSR_PR)
+
+/** Special Purpose Register declarations.
+ */
+#define SPR_XER         1               // Fixed point exception register.
+#define SPR_LR          8               // Link register.
+#define SPR_CTR         9               // Count register.
+#define SPR_DSISR       18              // DSI exception register.
+#define SPR_DAR         19              // Data access register.
+#define SPR_DEC         22              // Decrementer register.
+#define SPR_SRR0        26              // Save/restore register 0.
+#define SPR_SRR1        27              // Save/restore register 1.
+#define SPR_SPRG0       272             // SPR general 0.
+#define SPR_SPRG1       273             // SPR general 1.
+#define SPR_SPRG2       274             // SPR general 2.
+#define SPR_SPRG3       275             // SPR general 3.
+#define SPR_PVR         287             // Processor version register.
+
+#ifndef __ASSEMBLER__
+
+#if ELK_NAMESPACE
+#define outb __elk_outb
+#define inb __elk_inb
+#endif
+
+void outb(int, u_char);
+u_char inb(int);
+
+#endif // !__ASSEMBLER__
+
+#endif // !_cpu_h_
