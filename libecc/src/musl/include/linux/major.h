34de500d288e7ed8002c9a088f3e537ac6bd5fc2
Richard Pennington <rich@ellcc.org>
2016-01-04 00:06:02 +0000

    Added iproute2 to the Linux build rules.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@5776 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/musl/include/linux/major.h
+++ libecc/src/musl/include/linux/major.h
@@ -1,6 +1,13 @@
-/* Just enough to get ntfs-3g to compile.
+/* Just enough to get ntfs-3g and e2fsprogs to compile.
  */
 #ifndef _LINUX_MAJOR_H_
 #define _LINUX_MAJOR_H_
+
+#define HD_MAJOR 3
+#define LOOP_MAJOR 7
+#define SCSI_DISK0_MAJOR 8
+#define SCSI_CDROM_MAJOR 11
+#define SCSI_DISK1_MAJOR 65
+#define SCSI_DISK7_MAJOR 71
 
 #endif /* _LINUX_MAJOR_H_ */
