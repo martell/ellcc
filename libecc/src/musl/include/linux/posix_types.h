8f414dd6d053efedadd6891ce00f7581dc103a8a
Richard Pennington <rich@ellcc.org>
2016-01-09 20:27:52 +0000

    Build Das U-Boot on Linux.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@5835 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/musl/include/linux/posix_types.h
+++ libecc/src/musl/include/linux/posix_types.h
@@ -1,4 +1,4 @@
-/* Just enough to get iproute2 to compile.
+/* Just enough to get iproute2 and u-boot to compile.
  */
 #ifndef _LINUX_POSIX_TYPES_H_
 #define _LINUX_POSIX_TYPES_H_
@@ -6,6 +6,8 @@
 #include <asm/types.h>
 #ifndef __kernel_uid32_t
 typedef __u32 __kernel_uid32_t;
+typedef __u64 __kernel_ino_t;
+typedef int __kernel_daddr_t;
 #endif
 
 #endif /* _LINUX_POSIX_TYPES_H_ */
