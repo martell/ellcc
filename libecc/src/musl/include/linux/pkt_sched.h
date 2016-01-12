c567edae92e81ef6b0a18cb2db9a1d5b97456f7e
Richard Pennington <rich@ellcc.org>
2016-01-12 16:31:51 +0000

    Add dropbear build.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@5848 82857169-6e38-49d7-aaa8-07fb558f12fc

---
+++ libecc/src/musl/include/linux/pkt_sched.h
@@ -0,0 +1,9 @@
+/* Just enough to get dropbear to compile.
+ */
+#ifndef _LINUX_PKT_SCHED_H_
+#define _LINUX_PKT_SCHED_H_
+
+#define TC_PRIO_BULK 2
+#define TC_PRIO_INTERACTIVE 6
+
+#endif /* _LINUX_PKT_SCHED_H_ */
