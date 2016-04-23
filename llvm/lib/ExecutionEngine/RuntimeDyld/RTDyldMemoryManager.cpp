7691f628b263baa500ff895a584f80e38e01380c
Richard Pennington <rich@ellcc.org>
2016-04-23 20:38:08 +0000

    Unwind test.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@6194 82857169-6e38-49d7-aaa8-07fb558f12fc

--- llvm/lib/ExecutionEngine/RuntimeDyld/RTDyldMemoryManager.cpp
+++ llvm/lib/ExecutionEngine/RuntimeDyld/RTDyldMemoryManager.cpp
@@ -34,7 +34,8 @@ RTDyldMemoryManager::~RTDyldMemoryManager() {}
 
 // Determine whether we can register EH tables.
 #if (defined(__GNUC__) && !defined(__ARM_EABI__) && !defined(__ia64__) && \
-     !defined(__SEH__) && !defined(__USING_SJLJ_EXCEPTIONS__))
+     !defined(__SEH__) && !defined(__USING_SJLJ_EXCEPTIONS__)) && \
+     !defined(__mips__)
 #define HAVE_EHTABLE_SUPPORT 1
 #else
 #define HAVE_EHTABLE_SUPPORT 0
