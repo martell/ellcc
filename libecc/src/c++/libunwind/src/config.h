60e21be5a2cac89849472914737c4bc1833f4d28
Richard Pennington <rich@ellcc.org>
2016-04-23 20:51:57 +0000

    Unwind test.
    git-svn-id: http://ellcc.org/svn/ellcc/trunk@6195 82857169-6e38-49d7-aaa8-07fb558f12fc

--- libecc/src/c++/libunwind/src/config.h
+++ libecc/src/c++/libunwind/src/config.h
@@ -54,27 +54,6 @@
     #define _LIBUNWIND_SUPPORT_DWARF_UNWIND   1
     #define _LIBUNWIND_SUPPORT_DWARF_INDEX    0
   #endif
-
-#elif 0 && defined(__ELLCC__)
-  #ifdef __cplusplus
-    extern "C" {
-  #endif
-    void __assert_fail(const char *, const char *, int, const char *)
-                                                      __attribute__((noreturn));
-  #ifdef __cplusplus
-    }
-  #endif
-
-  #define _LIBUNWIND_BUILD_SJLJ_APIS      (0)
-  #define _LIBUNWIND_EXPORT               __attribute__((visibility("default")))
-  #define _LIBUNWIND_HIDDEN               __attribute__((visibility("hidden")))
-  #define _LIBUNWIND_LOG(msg, ...) fprintf(stderr, "libuwind: " msg, __VA_ARGS__)
-  #define _LIBUNWIND_ABORT(msg) __assert_fail(msg, __FILE__, __LINE__, __func__)
-  #define _LIBUNWIND_SUPPORT_COMPACT_UNWIND 0
-  #define _LIBUNWIND_SUPPORT_DWARF_UNWIND   1 // RICH: (!defined(__arm__))
-  #define _LIBUNWIND_SUPPORT_DWARF_INDEX    _LIBUNWIND_SUPPORT_DWARF_UNWIND
-  #include <string.h>   // For strlcpy().
-
 #else
   #include <stdlib.h>
 
